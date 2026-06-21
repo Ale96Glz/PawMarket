115fe953f6bcf3e8713a43a020ebfa37d2e2aa01
Alejandro González Osorio <aosorio961020@gmail.com>
2026-06-21 15:21:18 -0600

    Update OrdenMapper.java

--- src/main/java/com/bluesoft/pawmarket/mapper/OrdenMapper.java
+++ src/main/java/com/bluesoft/pawmarket/mapper/OrdenMapper.java
@@ -14,33 +14,29 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface OrdenMapper {
+
     @Mapping(target = "usuarioId", source = "usuario.id")
     @Mapping(target = "usuarioNombre", source = "usuario.nombre")
-    @Mapping(target = "estado", source = "estado")
-    @Mapping(target = "detalles", source = "detalles")
     OrdenResponseDTO toResponseDTO(Orden orden);
 
-    @Mapping(target = "productoId", source = "producto.id")
-    @Mapping(target = "productoNombre", source = "producto.nombre")
-    OrdenDetalleResponseDTO toDetallesResponseDTO(OrdenDetalle detalle);
-
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "orden", source = "orden")
-    @Mapping(target = "producto", source = "producto")
-    @Mapping(target = "cantidad", source = "dto.cantidad")
-    @Mapping(target = "precioUnitario", ignore = true)
-    @Mapping(target = "subtotal", ignore = true)
-    OrdenDetalle toDetalleEntity(OrdenDetalleRequestDTO dto, Producto producto, Orden orden);
-
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "usuario", source = "usuario")
+    @Mapping(target = "usuario", source = "cliente")
     @Mapping(target = "estado", ignore = true)
     @Mapping(target = "total", ignore = true)
     @Mapping(target = "detalles", ignore = true)
     @Mapping(target = "creadoEn", ignore = true)
     @Mapping(target = "actualizadoEn", ignore = true)
-    Orden toEntity(Usuario usuario);
+    Orden toEntity(Usuario cliente);
+
+    @Mapping(target = "productoId", source = "producto.id")
+    @Mapping(target = "productoNombre", source = "producto.nombre")
+    OrdenDetalleResponseDTO toDetalleResponseDTO(OrdenDetalle detalle);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cantidad", source = "dto.cantidad")
+    @Mapping(target = "precioUnitario", ignore = true)
+    @Mapping(target = "subtotal", ignore = true)
+    OrdenDetalle toDetalleEntity(OrdenDetalleRequestDTO dto, Producto producto, Orden orden);
 
     @AfterMapping
     default void defaultsAlCrear(@MappingTarget Orden orden) {
